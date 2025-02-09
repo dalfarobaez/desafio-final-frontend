586d793a25fdc11fac7d31ba0b018d490b4a55a9
pandaman404 <valenzuela.ahumada4@gmail.com>
2025-02-09 17:13:47 -0300

    feat: Add login form (WIP)

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,5 +1,90 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import FlexContainer from "../../components/layout/FlexContainer";
+import styled from "styled-components";
+import InputField from "../../components/inputField/InputField";
+import colors from "../../styles/colors";
+import device from "../../styles/breakpoints";
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Correo inválido.")
+    .required("Este campo es obligatorio."),
+  password: Yup.string()
+    .min(8, "La contraseña debe tener 8 caracteres.")
+    .matches(/[A-Z]/, "Debe contener al menos una mayúscula")
+    .matches(/^(?!\s+$).*$/, "No puede ser solo espacios en blanco")
+    .required("Este campo es obligatorio."),
+});
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const initialValues = { email: "", password: "" };
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log("Formulario enviado: ", values);
+    resetForm();
+  };
+  return (
+    <>
+      <FirstContainerStyled>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <h2>Iniciar sesión</h2>
+              <InputField
+                label="Email"
+                name="email"
+                placeholder="Ingrese su email"
+                showError={true}
+              />
+              <InputField
+                label="Contraseña"
+                name="password"
+                type="password"
+                placeholder="Ingrese su contraseña"
+                showError={true}
+              />
+              <InputField
+                label="Email"
+                name="email"
+                placeholder="Ingrese su email"
+                showError={true}
+              />
+            </Form>
+          )}
+        </Formik>
+      </FirstContainerStyled>
+    </>
+  );
 };
 
+const FirstContainerStyled = styled(FlexContainer)`
+  padding: 50px;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  width: 100%;
+  form {
+    h2 {
+      margin: 20px 0;
+    }
+    padding: 30px;
+    width: 100%;
+    max-width: 650px;
+    min-height: 300px;
+    background-color: ${colors.forkLight};
+    display: flex;
+    flex-flow: column nowrap;
+    /* gap: 20px; */
+    border-radius: 3px;
+    @media ${device.md} {
+      max-width: 650px;
+    }
+  }
+`;
 export default LoginPage;
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,11 +1,11 @@
 const colors = {
-  forkPrimary: '#d50032',
-  forkSecondary: '#bebebe',
-  forkSecondaryVariant: '#adadad',
-  forkLight: '#F5F5F5',
-  forkDark: '#282828',
-  forkBlack: '#000000',
-  forkWhite: '#FFFFFF',
+  forkPrimary: "#d50032",
+  forkSecondary: "#bebebe",
+  forkSecondaryVariant: "#adadad",
+  forkLight: "#F5F5F5",
+  forkDark: "#282828",
+  forkBlack: "#000000",
+  forkWhite: "#FFFFFF",
 };
 
 export default colors;
